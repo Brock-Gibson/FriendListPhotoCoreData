aead378e1680af8fd452437d8a592f367a717664
Brock Gibson <brockgibson@mwc-017237.dhcp.missouri.edu>
2019-03-18 11:59:43 -0500

    First Commit

---
+++ PhotosCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosCoreData
+//
+//  Created by Brock Gibson on 3/18/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
